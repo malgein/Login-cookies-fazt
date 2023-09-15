76692697176da0da3e5b2c00709f2ed561d1b1a2
Wilmer Pocaterra <malgein17@gmail.com>
2023-09-15 08:38:26 -0400

    feat/new route of register created successfully

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,11 +1,12 @@
 const User = require('../models/user.model')
 const bcrypt = require('bcryptjs')
+const createAccessToken = require('../libs/jwt.js')
 
 const register = async (req, res) => {
 		const { username, email, password} = req.body
 		// console.log(email, username, password)
 
-
+	//encripta el password
 		const passwordHash = await bcrypt.hash(password, 10)
 
 	try {
@@ -15,16 +16,23 @@ const register = async (req, res) => {
 			password: passwordHash
 		})
 		// console.log(newUser)
-
+		//El usuario que es creado pretende ser devuelto aunque no completo
 		const newUserSaved = await newUser.save()
+		//le pasamos el id del nuevo usuario para que la funcion importada de libs me cree su token
+		const token = await createAccessToken({id: newUserSaved._id})
+		//funcion que crea el token del usuario
+		res.cookie('token', token)
+			res.json({
+				id: newUserSaved._id,
+		 		username: newUserSaved.username,
+				email: newUserSaved.email
+		})
 
 		//Esta es la forma de obtener para el frontend solo los datos que necesito para el usuario
 		//el password no es necesario por eso se excluye del objeto
-		res.json({
-			id: newUserSaved.id,
-			username: newUserSaved.username,
-			email: newUserSaved.email
-		})
+		// res.json({
+
+		// })
 	} catch (error) {
 		console.log(error)
 	}
---
+++ src/libs/jwt.js
@@ -0,0 +1,35 @@
+// const TOKEN_SECRET = require('../config')
+
+// function createAccessToken(payload){
+// 	jwt.sign(
+// 		payload,
+// 		TOKEN_SECRET,
+//     { 
+//         id : newUserSaved._id 
+//     }, 
+//     'secret123',
+//     {
+//         expiresIn:'1d', 
+//     },
+//     (err, token) => {
+//         if(err) console.log(err)
+// 		}
+// 	)
+// }
+
+//Funcion que genera los tokens de acceso
+
+const TOKEN_SECRET = require('../config')
+const jwt = require("jsonwebtoken") 
+
+	//El payload es el id de un usuario medianto ello creara el token de acceso
+  const createAccessToken = async (payload) => {
+  return new Promise((resolve, reject) => {
+    jwt.sign(payload, TOKEN_SECRET, { expiresIn: "1d" }, (err, token) => {
+      if (err) reject(err);
+      resolve(token);
+    });
+  });
+}
+
+module.exports = createAccessToken
